0c7c7287b6a7ed47c52c88287887c5c3b3fe0be9
katherinestout <kjs14@hood.edu>
2019-02-05 11:41:02 -0500

    basic functionality

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -8,38 +8,85 @@ import Wrapper from './Wrapper';
 
 class Doctors extends Component {
     state = {
-        DoctorList
+        DoctorList,
+      //  doctors,
+        clickedArray: [],
+        loss: 0,
+        win: 0,
+        msg: "",
+        score: 0
     };
 
-  //if clicked dr who, add the id to array
+  //handleClick: click event, if clicked dr who, add the id to array
   handleClick = (id) => {
-    //const clickedArray = [''];
-    //clickedArray.push(id);
+    //if the array includes the id already, they add to lose
+    if (this.state.clickedArray.includes(id)){
+      this.setState({msg: "Sorry mate, you lost."});  
 
-      console.log(id);
-      //console.log(clickedArray);
+      let lose = this.state.loss +1;
+      let win = this.state.win;
 
-      //addClick();
+      win = 0;
+      this.setState({win: win, loss: lose, clickedArray: []});
+      this.shuffleDoctors();
+    } 
+    
+    else {
+      let win = this.state.win +1;
+      if (win === 12){
+        this.setState({msg: "YOU WON!"});
+        this.setState({win: 0, clickedArray: []});
+        this.setState({score: +1});
+      }
+    
+    
+    else {
+           //make clickedArray equal to that in the state
+    let clickedArray = this.state.clickedArray;
+    //push the id into the array
+    clickedArray.push(id); 
+
+    console.log(clickedArray);
+    this.setState({
+      msg: this.doctorMsg(),
+     // clickedArray: clickedDoctor,
+      win: win
+    });
+    this.shuffleDoctors();
+    }
+  }
   };
 
-    /*
-    addClick = () => {
-     
-      const clickedArray = [''];
-      clickedArray.push(id);
-  
-        console.log(id);
-        console.log(clickedArray);
-    };
- 
-    */
-    
-//console.log(this.state.DoctorList[1].name);
+  shuffleDoctors = () => {
+
+  }
+
+  doctorMsg = () => {
+    const msgArray = [
+      "Great Job!",
+      "Your memory is almost as good as mine, and I'm 2,000 years old!",
+      "Not bad for a human...",
+      "Allons y!",
+      "My stars!",
+      "Geronimo!"
+
+    ];
+    let random = Math.floor(Math.random() * msgArray.length);
+    return `${msgArray[random]}`;
+  };
+
   render() {
 
   
     return (
       <div>
+        <h1>
+          Score : 
+          Losses : {this.state.lose}
+          <br></br>
+         Doctor Count = {this.state.win}
+        </h1>
+        <h2>{this.state.msg}</h2>
           <div className="wrapping">
           <Wrapper>
         {this.state.DoctorList.map(DoctorList => (
